7f382b78cb624654a3e1eafba99001c4f7ab16d3
GRxAK <artkot777@gmail.com>
2022-12-16 01:03:20 +0600

    add seminar 01

---
+++ seminar_01/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+// int a = int.Parse(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Квадрат " + a + " = " + a * a);
+Console.WriteLine($"Квадрат {a}  = {a * a}");
