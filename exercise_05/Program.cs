cda004afc56d138967bcdef0c767ded99a732611
GRxAK <artkot777@gmail.com>
2022-12-19 22:58:01 +0600

    add exercise 05

---
+++ exercise_05/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int[] array = {0, 0, 0, 0, 0};
+int index = 0;
+
+int GoMaxArray() {
+    int max = 0;
+    Console.WriteLine("Заполните массив из пяти чисел, и программа найдет максимальное из них");
+    while (index < array.Length) {
+        Console.WriteLine("Введите число " + index);
+        array[index] = Convert.ToInt32(Console.ReadLine());
+        if (max < array[index]) {
+            max = array[index];
+        }
+        index++;
+    }
+    Console.WriteLine(string.Join(" ", array));
+    return max;
+}
+Console.WriteLine("Максисмальное число в массиве = " + GoMaxArray());
