d736f87cb5592e31ad7921ca5dffe0f00b8cbf2e
GRxAK <artkot777@gmail.com>
2022-12-23 01:30:15 +0600

    add Seminar #3

---
+++ seminar_03_a/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void PrintCord(int x, int y) {
+    if (x > 0 && y > 0) {
+        Console.WriteLine("I четверть");
+    } else if (x < 0 && y > 0) {
+        Console.WriteLine("II четверть");
+    } else if (x < 0 && y < 0) {
+        Console.WriteLine("III четверть");
+    } else if (x > 0 && y < 0) {
+        Console.WriteLine("VI четверть");
+    } else {
+        Console.WriteLine("Не входит ни в какую четверть");
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите координату х");
+int CordX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y");
+int CordY = Convert.ToInt32(Console.ReadLine());
+
+(PrintCord(CordX, CordY));
---
+++ seminar_03_b/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+
+string quarter = Console.ReadLine();
+
+switch (quarter) {
+
+    case "1":
+        Console.WriteLine("x от 0 и больше, y от 0 и больше");
+    break;
+    
+    case "2":
+        Console.WriteLine("x от 0 и меньше, y от 0 и больше");
+    break;
+    
+    case "3":
+        Console.WriteLine("x от 0 и меншье, y от 0 и меншье");
+    break;
+    
+    case "4":
+        Console.WriteLine("x от 0 и больше, y от 0 и меншье");
+    break;
+}
