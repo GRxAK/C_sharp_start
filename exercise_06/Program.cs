e34028053501ede464e428c196bd34714ce7acdf
GRxAK <artkot777@gmail.com>
2023-01-10 21:09:14 +0400

    add Exercise 06

---
+++ exercise_06/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+int[] array = {9, 665, 15, 1, 0, 9, 0, 47, 69, 25, 111, 561};
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] arr)
+{
+    for (int i=0; i < arr.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] > arr[maxPosition])
+            {
+                maxPosition = j;
+            }
+            int temporey = arr[i];
+            arr[i] = arr[maxPosition];
+            arr[maxPosition] = temporey;
+        }
+    }
+}
+
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
