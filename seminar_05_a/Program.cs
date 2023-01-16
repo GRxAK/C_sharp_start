14f48ad6616512ba330eda60e6896cfd7fb9fd1a
GRxAK <artkot777@gmail.com>
2023-01-16 23:12:06 +0400

    add Seminar #5 part-1

---
+++ seminar_05_a/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+Console.Clear();
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] arrayOne = CreateRandomArray(12, -9, 9);
+ShowArray(arrayOne);
+
+void Sum(int[] arr)
+{
+    int posetive = 0;
+    int negative = 0;
+    for(int i=0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        {
+            posetive += arr[i];
+        } else {
+            negative += arr[i];
+        }
+    }
+    Console.WriteLine($"Сумма положительныйх чисел = {posetive}");
+    Console.WriteLine($"Сумма от чисел = {negative}");
+}
+
+Sum(arrayOne);
---
+++ seminar_05_b/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+Console.Clear();
+
+int[] ArrOne(int leng)
+{
+    int[] arr = new int[leng];
+    for (int i=0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-99, 100);
+    }
+    return arr;
+}
+
+void ReversArr(int[] arr)
+{
+    Console.WriteLine($"[{String.Join(",",arr)}] ");
+    for (int i=0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+    Console.WriteLine($"[{String.Join(",",arr)}]");
+}
+ReversArr(ArrOne(4));
