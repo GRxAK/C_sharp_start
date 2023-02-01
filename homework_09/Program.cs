aa3a71d032f3ccc532505a78e663b32213d053cd
GRxAK <artkot777@gmail.com>
2023-02-01 23:47:37 +0400

    add Seminar and Homework #9

---
+++ homework_09/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.Clear();
+/* Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void ShowIntegers(int numb)
+{
+    if (numb == 0) return;
+    Console.Write($"{numb}  ");
+    ShowIntegers(numb - 1);
+    return;
+}
+
+/* Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNumber(int min, int max)
+{
+    int sum = 0;
+    if (min == max) return max;
+    sum = min + SumNumber(min + 1, max);
+    return sum;
+}
+
+/* Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int Ackermann(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if ((n != 0) && (m == 0)) return Ackermann(n - 1, 1);
+    else return Ackermann(n - 1, Ackermann(n, m - 1));
+}
+
+
+// запускаем задачки на выбор
+void GoHomework()
+{
+    Console.WriteLine("Введите норме задачки 1, 2, 3");
+    string lesson = Console.ReadLine();
+    Console.WriteLine();
+    switch (lesson)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("Программа выведет все натуральные числа в промежутке от 8 до 1.");
+            Console.WriteLine();
+            ShowIntegers(8);
+        break;
+        case "2":
+            Console.Clear();
+            Console.WriteLine("Программа найдёт сумму натуральных элементов в промежутке от 4 до 8.");
+            Console.WriteLine();
+            Console.WriteLine(SumNumber(4,8));
+            Console.WriteLine();
+        break;
+        case "3":
+            Console.Clear();
+            Console.WriteLine("Программа вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа 3 и 2.");
+            Console.WriteLine();
+            Console.WriteLine(Ackermann(3,2));
+            Console.WriteLine(); 
+        break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Попробуйте заного, такой задачи нет!");
+            GoHomework();
+        break;
+    }
+}
+
+GoHomework();
---
+++ seminar_09/Program.cs
@@ -0,0 +1,88 @@
+﻿Console.Clear();
+/* адайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void ShouwNumbs(int start,int end)
+{
+    Console.Write($"{start}  ");
+    if (start == end) return;
+    ShouwNumbs(start + 1, end);
+}
+
+void LessonOne ()
+{
+    Console.WriteLine("Введите число:");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    ShouwNumbs(0, numb);
+    Console.WriteLine();
+}
+
+// LessonOne();
+
+/* Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+
+
+void LessonTwo ()
+{
+    Console.WriteLine("");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+}
+
+// LessonTwo();
+
+/* Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int FunctionThree(int numb)
+{
+    int sum = 0;
+    if (numb == 0) 
+    {
+        return 0;
+    }
+    sum = numb % 10 + FunctionThree(numb / 10);
+    return sum;
+}
+
+void LessonThree ()
+{
+    Console.WriteLine("Read numb");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(FunctionThree(numb));
+    Console.WriteLine();
+}
+
+// LessonThree();
+
+/* Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+int Four(int a, int b)
+{
+    int c = 1;
+    if (b == 0) return c;
+    c = a * Four(a, b - 1);
+    return c;
+}
+
+void LessonFour ()
+{
+    // Console.WriteLine("");
+    // int numb = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine(Four(2,3));
+}
+
+// LessonFour();
+
